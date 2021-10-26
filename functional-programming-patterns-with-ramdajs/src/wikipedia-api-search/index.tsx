67efbe4cb9ee99c4d9d3a40647a603f8cba01813
kamilzielinskidev <kamilzielinskidev@gmail.com>
2021-10-26 21:23:36 +0200

    feature: add tailwind to wikipedia api search app

--- functional-programming-patterns-with-ramdajs/src/wikipedia-api-search/index.tsx
+++ functional-programming-patterns-with-ramdajs/src/wikipedia-api-search/index.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import "tailwindcss/tailwind.css";
+import "./styles.scss";
 
 const app = document.getElementById("app");
 ReactDOM.render(<App />, app);
