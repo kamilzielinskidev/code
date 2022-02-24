3e63b4d5d40cc381c236e5d38f1e81f47a09758a
kamilzielinskidev <kamilzielinskidev@gmail.com>
2022-02-24 10:13:35 +0100

    feature: improve responseState monad

--- heroes/src/common/response.spec.ts
+++ heroes/src/common/response.spec.ts
@@ -1,86 +1,51 @@
-const ResponseState = <T>(a: Promise<T>) => ({
-  loading: (cb: () => void) => {
+type ResponseStateM<A> = {
+  init: (cb: () => void) => ResponseStateM<A>;
+  map: <B>(cb: (a: A) => B) => ResponseStateM<B>;
+  chain: <B>(cb: (a: A) => B) => Promise<B>;
+  catch: <B>(cb: (a: Error) => B) => ResponseStateM<A | B>;
+};
+
+const ResponseState = <A>(a: Promise<A>): ResponseStateM<A> => ({
+  init: (cb) => {
     cb();
     return ResponseState(a);
   },
-  then: <A>(cb: (a: T) => A) => ResponseState(a.then(cb)),
-  catch: <A>(cb: (a: Error) => A) => ResponseState(a.catch(cb)),
+  map: (cb) => ResponseState(a.then(cb)),
+  chain: (cb) => a.then(cb).catch(cb),
+  catch: (cb) => ResponseState(a.catch(cb)),
 });
 
-it("should call loading fn on init", () => {
-  const mockClass = {
-    loading: () => {},
-  };
-  const loading = jest.spyOn(mockClass, "loading");
+describe("Chained ResponseState", () => {
+  it("should call init on start before any other", async () => {
+    const mockInitFn = jest.fn();
+    const mockMapFn = jest.fn().mockImplementation((x) => x + 1);
+    await ResponseState(Promise.resolve(1)).init(mockInitFn).map(mockMapFn);
 
-  ResponseState(Promise.resolve()).loading(mockClass.loading);
-  expect(loading).toHaveBeenCalled();
+    expect(mockInitFn.mock.invocationCallOrder[0]).toBeLessThan(
+      mockMapFn.mock.invocationCallOrder[0]
+    );
+  });
+
+  it("should map the resolved value to another ResponseState", async () => {
+    const mockMapFn = jest.fn().mockImplementation((x) => x + 1);
+    await ResponseState(Promise.resolve(1)).map(mockMapFn);
+
+    expect(mockMapFn).toReturnWith(2);
+  });
+
+  it("should catch the rejected value to another ResponseState", async () => {
+    const mockMapFn = jest.fn().mockImplementation((x) => x + "error");
+
+    await ResponseState(Promise.reject("error1")).catch(mockMapFn);
+
+    expect(mockMapFn).toReturnWith("error1error");
+  });
+
+  it("should chain the resolved or rejected value to simple promise", () => {
+    fail("TODO");
+  });
+
+  it("should chain fns properly", () => {
+    fail("TODO");
+  });
 });
-
-it("should call map fn on resolved", async () => {
-  const mockClass = {
-    then: (a: number) => a + 1,
-  };
-  const then = jest.spyOn(mockClass, "then");
-
-  await ResponseState(Promise.resolve(1)).then(mockClass.then);
-
-  expect(then).toReturnWith(2);
-});
-
-it("should call catch on rejected", async () => {
-  const mockClass = {
-    catched: (a: Error) => a.message,
-  };
-
-  const catched = jest.spyOn(mockClass, "catched");
-
-  await ResponseState(Promise.reject({ message: "test1" })).catch(
-    mockClass.catched
-  );
-
-  expect(catched).toReturnWith("test1");
-});
-
-it("should chain the fns properly", async () => {
-  const mockClass = {
-    init: () => {},
-    then1: (a: number) => a + 1,
-    then2: (a: number) => a + 10,
-    then3: (a: number) => {
-      throw new Error("test");
-    },
-    then4: (a: number) => a + 100,
-    catched: (a: Error) => a.message,
-  };
-
-  const init = jest.spyOn(mockClass, "init");
-  const then1 = jest.spyOn(mockClass, "then1");
-  const then2 = jest.spyOn(mockClass, "then2");
-  const then4 = jest.spyOn(mockClass, "then4");
-  const catched = jest.spyOn(mockClass, "catched");
-
-  await ResponseState(Promise.resolve(1))
-    .loading(mockClass.init)
-    .then(mockClass.then1)
-    .then(mockClass.then2)
-    .then(mockClass.then3)
-    .then(mockClass.then4)
-    .catch(mockClass.catched);
-
-  expect(init.mock.invocationCallOrder[0]).toBeLessThan(
-    then1.mock.invocationCallOrder[0]
-  );
-
-  expect(then1.mock.invocationCallOrder[0]).toBeLessThan(
-    then2.mock.invocationCallOrder[0]
-  );
-
-  expect(then2).toReturnWith(12);
-  expect(then4).not.toBeCalled();
-  expect(catched).toReturnWith("test");
-});
-
-it("should contain value", () => {});
-it("should map the value by map function", () => {});
-it("should have chain fn instead of then", () => {});
