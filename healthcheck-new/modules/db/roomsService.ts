3c9106555ad03decfd55fb30d59974bca1c99f0d
kamilzielinskidev <kamilzielinskidev@gmail.com>
2022-04-08 20:36:01 +0200

    healthcheck: separate legacy projects

--- healthcheck-new/modules/db/roomsService.ts
+++ healthcheck-new/modules/db/roomsService.ts
@@ -12,5 +12,5 @@ export const getById = (id: string) => pipe(connect(), andThen(findOneById(id)))
 export const updateById = (id: string, value: Partial<RoomsSchema>) =>
   pipe(connect(), andThen(updateOneById(value, id)));
 
-export const createRoom = (name: string) =>
+export const createRoom = (name: RoomsSchema["name"]) =>
   pipe(connect(), andThen(insertOne({ name, surveyId: null, survey_isOpen: false, survey_iteration: null })));
