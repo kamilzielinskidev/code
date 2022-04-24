f61e96c17aa26b2f66a1b0413847c528b00af513
kamilzielinskidev <kamilzielinskidev@gmail.com>
2022-04-24 20:16:22 +0200

    healtcheck: answer for survey page

---
+++ healthcheck-new/__tests__/pages/survey/[id].spec.ts
@@ -0,0 +1,18 @@
+describe("On Survey page", () => {
+  describe("When there is no user in app store", () => {
+    it.todo("should navigate to Home page");
+  });
+  describe("When there is a user in app store", () => {
+    it.todo("should render room name");
+    it.todo("should render back button");
+    it.todo("should render rating component for each category");
+    it.todo("should render submit button");
+  });
+});
+
+describe("When use submits rate all categories", () => {
+  describe("When user submits the survey", () => {
+    it.todo("should send proper request to backend");
+    it.todo("should redirect to room page");
+  });
+});
--- healthcheck-new/pages/survey/[id].tsx
+++ healthcheck-new/pages/survey/[id].tsx
@@ -1,9 +1,106 @@
+import axios from 'axios';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useMutation } from 'react-query';
 
-const SurveyPage: NextPage = () => {
-  const { query } = useRouter();
-  return <div>{query.id}</div>;
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import HealingIcon from '@mui/icons-material/Healing';
+import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
+import { Button, CircularProgress, Grid, IconButton, Rating, Typography } from '@mui/material';
+
+import { authState } from '../../modules/auth/lib/state';
+import { ResponsesSchema } from '../../modules/db/lib/schemas';
+import { useQueryRoom } from '../../modules/room/lib/hooks/useQueryRoom';
+
+const CATEGORIES = ["FUN", "HEALTH_OF_CODEBASE"] as const;
+
+const categoryToIcon = {
+  FUN: VideogameAssetIcon,
+  HEALTH_OF_CODEBASE: HealingIcon,
 };
 
-export default SurveyPage;
+const Survey: NextPage = () => {
+  const { query, push, back } = useRouter();
+  const { user } = authState();
+  const funRate = useState(1);
+  const healthOfCodebaseRate = useState(1);
+  const { data: room } = useQueryRoom(query.id as string);
+
+  const { mutate: answer } = useMutation<ResponsesSchema>("response", () =>
+    axios
+      .post(`/api/responses`, {
+        surveyId: query.id,
+        username: user!.name,
+        answers: {
+          FUN: funRate[0],
+          HEALTH_OF_CODEBASE: healthOfCodebaseRate[0],
+        },
+      })
+      .catch((err) => {
+        throw err.response.data.msg;
+      })
+      .then((res) => res.data)
+  );
+
+  const categoryToState = { FUN: funRate, HEALTH_OF_CODEBASE: healthOfCodebaseRate };
+
+  useEffect(() => {
+    if (!user) {
+      push("/");
+    }
+  }, []);
+
+  return !user || !room ? (
+    <CircularProgress />
+  ) : (
+    <Grid container>
+      <Grid item container direction="column" padding="1rem 0">
+        <IconButton sx={{ alignSelf: "self-start" }} onClick={back}>
+          <ArrowBackIcon />
+        </IconButton>
+        <Typography variant="h1" fontSize="1.5rem">
+          {room.name}
+        </Typography>
+        <Typography variant="h3" fontSize="1rem">
+          {room._id.toString()}.
+        </Typography>
+      </Grid>
+      <Grid item container direction="column">
+        {CATEGORIES.map((category) => {
+          const Icon = categoryToIcon[category];
+          return (
+            <Grid key={category} container alignItems="center" justifyContent="space-between" wrap="nowrap">
+              <Grid item container wrap="nowrap">
+                <Icon fontSize="large" />
+                <Typography fontSize="1.5rem" variant="body1">
+                  {category}
+                </Typography>
+              </Grid>
+              <Rating
+                size="large"
+                value={categoryToState[category][0]}
+                max={3}
+                onChange={(_, newValue) => {
+                  categoryToState[category][1](newValue!);
+                }}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+      <Button
+        sx={{ mt: "1rem" }}
+        variant="contained"
+        onClick={() => {
+          answer();
+          back();
+        }}
+      >
+        Submit
+      </Button>
+    </Grid>
+  );
+};
+
+export default Survey;
